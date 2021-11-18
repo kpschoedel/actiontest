dfd602984fed87e1895ac6eb8837b3bc995e53b4
Kevin Schoedel <kpschoedel@google.com>
2021-11-18 16:29:37 -0500

    scripts 2

--- gh_size_env.sh
+++ gh_size_env.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
-MERGE_PARENT="$(git --no-pager log -1 --pretty=tformat:%s | sed -n -e 's/Merge [0-9a-f]\+ into //p')"
-if [[ -z "${MERGE_PARENT}" || "${GH_EVENT_PR}" == 0 ]]
-then
-  MERGE_PARENT=$GH_EVENT_BASE
-fi
-echo "GH_EVENT_PARENT=$MERGE_PARENT" >>"$GITHUB_ENV"
+{
+  echo "GH_EVENT_PR='$GH_EVENT_PR'"
+  echo "GH_EVENT_HASH='$GH_EVENT_HASH'"
+  echo "GH_EVENT_BASE='$GH_EVENT_BASE'"
+
+  MERGE_PARENT="$(git --no-pager log -1 --pretty=tformat:%s | sed -n -e 's/Merge [0-9a-f]\+ into //p')"
+  if [[ -z "${MERGE_PARENT}" || "${GH_EVENT_PR}" == 0 ]]
+  then
+    MERGE_PARENT=$GH_EVENT_BASE
+  fi
+  echo "GH_EVENT_PARENT='$MERGE_PARENT'"
+} >>"$GITHUB_ENV"
